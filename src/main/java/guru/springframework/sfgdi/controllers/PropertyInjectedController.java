120b23a2dbe9d2176ca3c5f3bcbdecba9811e6a3
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-06-16 15:42:55 -0600

    Dependency Injection With Spring Framework

--- src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,12 +1,16 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by gd on 6/16/2023
  */
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting(){
--- src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by gd on 6/16/2023
  */
+@Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
