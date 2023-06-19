7a0ab0a4a2933d28d33b4c9e2da22f9a7e157228
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-06-19 15:36:30 -0600

    Spring Default Profiles

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by gd on 6/19/2023
  */
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
 
