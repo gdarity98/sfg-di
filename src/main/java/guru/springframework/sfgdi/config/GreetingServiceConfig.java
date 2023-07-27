bd7e87be4fffa97726fe4de48d91d7c1d8085ecb
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-07-27 15:54:11 -0600

    Primary Bean and Profile with Java Configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,16 +1,35 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by gd on 7/27/2023
  */
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES","default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService I18nSpanishGreetingService(){
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -1,14 +1,8 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 /**
  * Created by gd on 6/19/2023
  */
-@Profile({"ES", "default"})
-@Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
 
     @Override
