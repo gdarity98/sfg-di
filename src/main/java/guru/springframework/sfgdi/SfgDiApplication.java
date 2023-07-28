ef928d9697c1bbeabc33313474717535ace1fdf4
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-07-28 15:59:18 -0600

    Spring XML Config

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
+//@ImportResource("classpath:sfgdi-config.xml")
 @SpringBootApplication
 public class SfgDiApplication {
 
